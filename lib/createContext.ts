f3d269167bfaab4482088a73dc8062dba44e7503
Nathan Faucett <nathanfaucett@gmail.com>
2018-09-08 12:49:08 -0400

    fix partial connect

--- lib/createContext.ts
+++ lib/createContext.ts
@@ -1,13 +1,13 @@
 import * as React from "react";
 
-export type IMapFn<IP, OP, S> = (state: S, props: IP) => OP;
+export type IMapFn<S, A, B> = (state: S, props: A) => B;
 
 export function createContext<S>(state: S) {
     const { Provider, Consumer } = React.createContext(state);
 
-    const connect = <A = {}, B = {}>(
-        map: IMapFn<Partial<B>, Partial<A>, S>
-    ) => (Component: React.ComponentClass<A>): React.ComponentClass<B> => {
+    const connect = <A = {}, B = {}>(map: IMapFn<S, B, Partial<A>>) => (
+        Component: React.ComponentClass<A>
+    ): React.ComponentClass<B> => {
         return class Connect extends React.PureComponent<B> {
             static displayName = `Connect(${Component.displayName ||
                 Component.name ||
@@ -29,7 +29,7 @@ export function createContext<S>(state: S) {
                         React.createElement(Component as any, {
                             ref: this.componentRef,
                             ...(<{}>props),
-                            ...(<{}>map(state, (props as any) as Partial<B>))
+                            ...(<{}>map(state, props))
                         })
                 });
             }
